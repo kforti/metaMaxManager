e1dd1caa58249908d4c15b44b33a2b27593e1f9f
Kevin <kevin.r.fortier@gmail.com>
2019-10-10 17:30:07 -0400

    working micromanager app

---
+++ metaMaxManager/MetaMaxManager/src/org/micromanager/metamaxmanager/model/Model.java
@@ -0,0 +1,32 @@
+package org.micromanager.metamaxmanager.model;
+
+public class Model {
+
+    String base_url = "https://api.particle.io/v1/devices/";
+    String access_token = "b0b4abf352e4813771428d70189ad7b128a5fba3";
+    String led_state = "off";
+    String device_id = "4b001c001851373237343331";
+
+    public void Model(){
+        this.led_state = "off";
+    }
+
+    public String ledState(){
+        return led_state;
+    }
+
+    public void flipLedState(){
+        if (led_state.equalsIgnoreCase("on")) {
+            led_state = "off";
+        }
+        else if (led_state.equalsIgnoreCase("off")){
+            led_state = "on";
+        }
+    }
+
+    public String generateFunctionURL(String function) {
+        String url = base_url + device_id + "/" + function + "?" + "access_token=" + access_token;
+        return url;
+    }
+
+}
---
+++ metaMaxManager/push.sh
@@ -0,0 +1,5 @@
+
+# move jar to imageJ plugins location
+sudo cp /home/kevin/metaMaxManager/metaMaxManager/helloWorldPlugin.jar /usr/local/ImageJ/mmplugins/
+# Start Micromanager
+/usr/local/ImageJ/mmimagej 2>&1 >/dev/null &
